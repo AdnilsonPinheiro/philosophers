6034dde6f06747d18f2cf7f6119f3dde7787cef6
AdnilsonPinheiro <adpmaia@gmail.com>
2026-01-02 17:59:56 +0000

    updated times greater than time_to_die

--- philo/src/routine.c
+++ philo/src/routine.c
@@ -6,7 +6,7 @@
 /*   By: adpinhei <adpinhei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/26 17:41:15 by adpinhei          #+#    #+#             */
-/*   Updated: 2026/01/02 17:14:41 by adpinhei         ###   ########.fr       */
+/*   Updated: 2026/01/02 17:58:52 by adpinhei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,10 @@ void	*routine(void *args)
 	philo = (t_philo *)args;
 	left_fork = philo->philo_id - 1;
 	right_fork = philo->philo_id % philo->table->philo_number;
+	if (philo->table->time_to_eat > philo->table->time_to_die)
+		philo->table->time_to_eat = philo->table->time_to_die;
+	if (philo->table->time_to_sleep > philo->table->time_to_die)
+		philo->table->time_to_sleep = philo->table->time_to_die;
 	routine_loop(philo, left_fork, right_fork);
 	return (NULL);
 }
